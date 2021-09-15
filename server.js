61e042c2536018bcc748ac6d794581aee97b4aa8
cnicacio <carlosnicacio94@gmail.com>
2021-09-15 19:29:22 -0300

    Cors config

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 require("express-async-errors");
 var cors = require("cors");
 const connectToDb = require("./src/database/database");
 const router = require("./src/routes/routes");
 
-
 connectToDb();
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
-
 app.use(express.json());
+app.use(cors());
+app.options("*", cors());
 app.use(router);
 
 app.listen(port, () =>
